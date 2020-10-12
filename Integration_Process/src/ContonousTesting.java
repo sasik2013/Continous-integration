e3c746ea385aedc63a7421cb506f32c7af21385c
shri <shri@DESKTOP-RJUFK94>
2020-10-12 21:43:58 +0530

    newclass

---
+++ Integration_Process/src/ContonousTesting.java
@@ -0,0 +1,16 @@
+
+public class ContonousTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("Sasi");
+		System.out.println("kumar");
+		System.out.println("chennai");
+		System.out.println("SoftwareTesteng");
+		
+	}
+
+}
